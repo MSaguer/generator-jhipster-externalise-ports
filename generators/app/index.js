92e101da1a2dc11a4c52514e44e6a3a2d4ae32d0
Marc Saguer <saguermarc@gmail.com>
2018-10-27 22:42:26 +0200

    Fix test and rename settings variables

--- generators/app/index.js
+++ generators/app/index.js
@@ -47,25 +47,6 @@ module.exports = class extends BaseGenerator {
         };
     }
 
-    // prompting() {
-    //     const prompts = [
-    //         {
-    //             type: 'input',
-    //             name: 'm2path',
-    //             message: 'Please write your .m2 absolute path',
-    //             default: `${process.env.USERPROFILE}/.m2`
-    //         }
-    //     ];
-
-    //     const done = this.async();
-    //     this.prompt(prompts).then((props) => {
-    //         this.props = props;
-    //         // To access props later use this.props.someOption;
-
-    //         done();
-    //     });
-    // }
-
     writing() {
         // function to use directly template
         this.template = function (source, destination) {
@@ -83,25 +64,25 @@ module.exports = class extends BaseGenerator {
                     file: applicationDevYml,
                     pattern: /(port: 8080)/g,
                     regex: true,
-                    content: 'port: #spring.server.port#'
+                    content: 'port: #server.port#'
                 }, this);
                 jhipsterUtils.replaceContent({
                     file: applicationDevYml,
                     pattern: /(port: 25)/g,
                     regex: true,
-                    content: 'port: #spring.mail.port#'
+                    content: 'port: #mail.port#'
                 }, this);
                 jhipsterUtils.replaceContent({
                     file: applicationDevYml,
                     pattern: /(port: 5000)/g,
                     regex: true,
-                    content: 'port: #spring.logging.logstash.port#'
+                    content: 'port: #logging.logstash.port#'
                 }, this);
                 jhipsterUtils.replaceContent({
                     file: applicationDevYml,
                     pattern: /base-url: (http:\/\/127\.0\.0\.1:8080)\n/g,
                     regex: true,
-                    content: 'base-url: http://127.0.0.1:#spring.server.port#\n'
+                    content: 'base-url: http://127.0.0.1:#server.port#\n'
                 }, this);
             } catch (e) {
                 this.log(chalk.yellow('\nUnable to find ') + applicationDevYml + chalk.yellow(' is not updated\n'));
@@ -116,19 +97,19 @@ module.exports = class extends BaseGenerator {
                     file: applicationTestYml,
                     pattern: /(port: 10344)/g,
                     regex: true,
-                    content: 'port: #server.port#'
+                    content: 'port: #test.server.port#'
                 }, this);
                 jhipsterUtils.replaceContent({
                     file: applicationTestYml,
                     pattern: /(port: 5000)/g,
                     regex: true,
-                    content: 'port: #spring.logging.logstash.port#'
+                    content: 'port: #logging.logstash.port#'
                 }, this);
                 jhipsterUtils.replaceContent({
                     file: applicationTestYml,
                     pattern: /base-url: (http:\/\/127\.0\.0\.1:8080)\n/g,
                     regex: true,
-                    content: 'base-url: http://127.0.0.1:#spring.server.port#\n'
+                    content: 'base-url: http://127.0.0.1:#server.port#\n'
                 }, this);
             } catch (e) {
                 this.log(chalk.yellow('\nUnable to find ') + applicationTestYml + chalk.yellow(' is not updated\n'));
@@ -183,19 +164,29 @@ module.exports = class extends BaseGenerator {
             }
         };
 
+        this.updateApplicationTest = function () {
+            this.template('mail/testEmail.html', `${jhipsterConstants.SERVER_TEST_RES_DIR}templates/mail/testEmail.html`);
+            const testMail = `${jhipsterConstants.SERVER_TEST_SRC_DIR}${this.packageFolder}/service/MailServiceIntTest.java`;
+            try {
+                jhipsterUtils.replaceContent({
+                    file: testMail,
+                    pattern: /, http:\/\/127\.0\.0\.1:8080,/g,
+                    regex: true,
+                    content: ','
+                }, this);
+            } catch (e) {
+                this.log(chalk.yellow('\nUnable to find ') + testMail + chalk.yellow(' is not updated\n'));
+                this.debug('Error:', e);
+            }
+        };
 
-        // // variable from questions
-        // this.m2path = this.props.m2path;
-
-        // // show all variables
-        // this.log('\n--- Externalize path ---');
-        // this.log(`m2path=${this.m2path}`);
+        this.packageFolder = this.jhipsterAppConfig.packageFolder;
 
         this.updateApplicationDevYml();
         this.updateApplicationTestYml();
         this.updatePom();
-        this.template('settings.xml', 'settings.xml');
-
+        this.template('settings.exml', 'settings.xml');
+        this.updateApplicationTest();
 
     }
 
